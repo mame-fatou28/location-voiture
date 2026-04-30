5f5049042a6635a688ee482d5dc68afe449d3357
mame-fatou28 <mamefatousene08@gmail.com>
2026-04-30 12:32:11 +0000

    modification de a_propos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 
 import './App.css'
+import A_propos from './component/A_propos'
 
 function App() {
 
 
   return (
     <>
-      
+      <A_propos/>
     </>
   )
 }
